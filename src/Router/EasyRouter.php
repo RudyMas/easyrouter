019bb5976d0a2664900e51b81b1ec729882f0097
Rudy Mas <rudy.mas@rudymas.be>
2017-07-11 11:59:41 +0200

    Switched parameters to load Repository.

--- src/Router/EasyRouter.php
+++ src/Router/EasyRouter.php
@@ -14,7 +14,7 @@ use RudyMas\PDOExt\DBconnect;
  * @author      Rudy Mas <rudy.mas@rmsoft.be>
  * @copyright   2016-2017, rmsoft.be. (http://www.rmsoft.be/)
  * @license     https://opensource.org/licenses/GPL-3.0 GNU General Public License, version 3 (GPL-3.0)
- * @version     0.7.5
+ * @version     0.7.6
  * @package     RudyMas\Router
  */
 class EasyRouter
@@ -165,7 +165,7 @@ class EasyRouter
                         if (!empty($value['repositories'])) {
                             foreach ($value['repositories'] as $repositoryToLoad) {
                                 $repository = '\\Repository\\' . $repositoryToLoad . 'Repository';
-                                $arguments[] = new $repository(null, $this->db);
+                                $arguments[] = new $repository($this->db, null);
                             }
                         }
                         $arguments[] = $variables;
